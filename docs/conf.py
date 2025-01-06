7a86da4b38fe6318bc5cddc9fd315266df651004
Vallari Shende <66207248+VallariS@users.noreply.github.com>
2025-01-06 15:27:23 -0500

    Update conf.py - switching to book theme

--- docs/conf.py
+++ docs/conf.py
@@ -56,10 +56,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
-html_theme_options = {
-    "body_min_width": "60%",
-    "body_max_width": "75%"}
+html_theme = "Book"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
