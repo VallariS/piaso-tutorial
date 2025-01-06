a435d5ef242aa5a30ee116bd06f3837359ce568d
Vallari Shende <66207248+VallariS@users.noreply.github.com>
2025-01-06 15:24:49 -0500

    Update conf.py - adjusting body width

--- docs/conf.py
+++ docs/conf.py
@@ -56,9 +56,10 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "basic"
+html_theme = "sphinx_rtd_theme"
 html_theme_options = {
-    "body_max_width": "1200"}
+    "body_min_width": "60%",
+    "body_max_width": "75%"}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
